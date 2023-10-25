9dc2d8c4d29570a376a5e5d552373b3ef5c2fb4f
keane <keaneathallah960@gmail.com>
2023-10-25 18:07:02 +0800

    menyelesaikan reusable api call

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,9 @@
-import AnimeList from "@/components/AnimeList";
-import Header from "@/components/AnimeList/Header";
+import AnimeList from "@/components/AnimeList"
+import Header from "@/components/AnimeList/Header"
+import getAnimeResponse from "./libs/api-libs"
+
 const Page = async () => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`
-  );
-  const topAnime = await response.json();
+  const topAnime = await getAnimeResponse("top/anime", "limit=8")
   return (
     <>
       {/* Anime Populer */}
@@ -17,6 +16,6 @@ const Page = async () => {
         <AnimeList api={topAnime} />
       </section>
     </>
-  );
-};
-export default Page;
+  )
+}
+export default Page
--- src/app/populer/page.jsx
+++ src/app/populer/page.jsx
@@ -3,16 +3,14 @@ import AnimeList from "@/components/AnimeList"
 import HeaderMenu from "@/components/Utilities/HeaderMenu/HeaderMenu"
 import Pagination from "@/components/Utilities/HeaderMenu/Pagination"
 import React, { useEffect, useState } from "react"
+import getAnimeResponse from "../libs/api-libs"
 
 const Page = () => {
   const [page, setPage] = useState(1)
   const [topAnime, setTopAnime] = useState([])
   const fetchData = async () => {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?page=${page}`
-    )
-    const data = await response.json()
-    setTopAnime(data)
+    const popularAnime = await getAnimeResponse("top/anime", `page=${page}`)
+    setTopAnime(popularAnime)
   }
 
   useEffect(() => {
