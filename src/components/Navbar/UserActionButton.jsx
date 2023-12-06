b14d6fbf6da4cd6426b71502010241324cf6fa84
keane <keaneathallah960@gmail.com>
2023-12-07 01:10:05 +0800

    add Dashboard on route users/dashboard

--- src/components/Navbar/UserActionButton.jsx
+++ src/components/Navbar/UserActionButton.jsx
@@ -5,13 +5,20 @@ const UserActionButton = async () => {
   const actionLabel = user ? "Sign Out" : "Sign In"
   const actionURL = user ? "/api/auth/signout" : "/api/auth/signin"
   return (
-    <>
+    <div className="flex gap-2 justify-between">
+      {user ? (
+        <Link
+          href="/users/dashboard"
+          className="bg-color-primary p-2 rounded text-color-secondary py-2 px-8 ">
+          Dashboard
+        </Link>
+      ) : null}
       <Link
         href={actionURL}
-        className="bg-color-accent p-2 rounded text-color-dark text-font-bold">
+        className="bg-color-dark p-2 rounded text-color-accent py-2 px-8 inline-block">
         {actionLabel}
       </Link>
-    </>
+    </div>
   )
 }
 
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,7 +5,9 @@ const Navbar = () => {
   return (
     <header className="bg-color-accent">
       <div className="flex md:flex-row flex-col justify-between md:items-center  p-4 gap-2">
-        <Link className="font-bold text-color-dark text-2xl" href="/">
+        <Link
+          className="bg-color-secondary font-bold text-2xl p-5 rounded-2xl text-color-accent"
+          href="/">
           KEANIMELIST
         </Link>
         <InputSearch />
